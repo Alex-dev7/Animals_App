b706c69367cee7b69337273461f9d3a935e16f66
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-29 20:18:14 -0500

    post routes for the user controllers

--- controllers/user.js
+++ controllers/user.js
@@ -18,8 +18,15 @@ router.get('/signup', (req, res) => {
 })
 
 //post => submit form
-router.post('/signup', (req, res) => {
-    res.send('signup')
+router.post('/signup', async (req, res) => {
+    // encrypt password
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+    //create new user
+    User.create(req.body, (err, user) => {
+    //redirect to login page
+    res.redirect('/user/login')
+    })
+    
 })
 
 // the login routes
@@ -30,7 +37,22 @@ router.get('/login', (req, res) => {
 
 //post => submit form
 router.post('/login', (req, res) => {
-    res.send('login')
+    // get the data from the request body
+    const {username, password} = req.body
+    User.findOne({username}, (err, user) => {
+        //checking if user exists
+        if(!user) {
+            res.send(`user doesn't exists`)
+        } else {
+            // checking if password matches
+            const result = bcrypt.compareSync(password, user.password)
+            if(result) {
+                res.redirect('/animals')
+            } else {
+                res.send('wrong password')
+            }
+        }
+    })
 })
 
 
