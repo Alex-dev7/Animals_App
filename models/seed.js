238b34ec63e64714eb9a61029519279610b7eb6c
Alexei Rusu <rusualexei1995@gmail.com>
2022-11-30 18:09:39 -0500

    last commit for now)

--- models/seed.js
+++ models/seed.js
@@ -9,9 +9,11 @@ mongoose.connection.on('open', () => {
 
     // array of starter fruits
     const startingAnimal = [
-        { species: "Lion", extinct: false, location: "Africa", lifeExpectancy: 16, img: "https://www.worldatlas.com/r/w960-q80/upload/3f/94/aa/shutterstock-126767138.jpg"},
-        { species: "Lion", extinct: true, location: "Africa", lifeExpectancy: 16 },
-        { species: "Lion", extinct: false, location: "Africa", lifeExpectancy: 16 }
+        { species: "Raccoon", extinct: false, location: "North America", lifeExpectancy: 5, img: "https://i.insider.com/5086bc69eab8eae57a000000?width=750&format=jpeg&auto=webp"},
+        { species: "Fox", extinct: true, location: "Europe and North America", lifeExpectancy: 4, img: "https://www.niabizoo.com/wp-content/uploads/2018/05/ms-animals-habitats-mammals.jpg" },
+        { species: "Tiger", extinct: false, location: "Asia", lifeExpectancy: 10, img: "https://www.dartmoorzoo.org.uk/wp-content/uploads/2021/01/Tiger-1.jpg" },
+        { species: "Chimpanzee", extinct: false, location: "Africa", lifeExpectancy: 15, img: "https://cdn.mos.cms.futurecdn.net/26MWT6NHVvQMbLKpmnMjDR.jpg" }
+
     ]
 
     // delete all animals
